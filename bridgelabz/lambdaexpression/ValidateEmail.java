e0264b99380521ae170a35b6f76aa37b1d6179de
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 17:56:53 +0530

    ValidateEmail.java

--- bridgelabz/lambdaexpression/ValidateEmail.java
+++ bridgelabz/lambdaexpression/ValidateEmail.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.lambdaexpression;
 
-public class ValidateEmail {
+public class ValidateEmail 
+{
 	 public static boolean validateEmail(String email) throws UserRegistrationMain {
 		return false;
 	}
