0b9ec36de94660717fc407f4d00875b6fd259924
bhartifule <116824470+bhartifule@users.noreply.github.com>
2023-06-14 17:59:37 +0530

    Update ValiudateLastName.java

--- bridgelabz/lambdaexpression/ValiudateLastName.java
+++ bridgelabz/lambdaexpression/ValiudateLastName.java
@@ -1,7 +1,8 @@
 package com.bridgelabz.lambdaexpression;
 
 public class ValiudateLastName {
-    public static boolean validateLastName(String firstName) throws UserRegistrationMain {
+    public static boolean validateLastName(String firstName) throws UserRegistrationMain
+ {
 		return false;
 	}
 
